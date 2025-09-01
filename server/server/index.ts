321013d70762b302447586cd1023b2d597ac35b2
Shaankhan1992 <sarfarajk7890@gmail.com>
2025-09-01 13:32:17 +0530

    Create index.ts

---
+++ server/server/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import routes from "./routes";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", routes);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
